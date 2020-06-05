5d1fcef63838a933e2169067e3580d5af319f07f
DaleNaci <dale.nash01@gmail.com>
2020-06-05 16:04:34 -0400

    Added help message

--- bot/Main.py
+++ bot/Main.py
@@ -39,6 +39,26 @@ async def smp(ctx):
     await ctx.send(embed=embed)
 
 
+@bot.command()
+async def help(ctx):
+    command_descs = {
+        "!help": "Provides information about all commands.",
+        "!smp": "List users currently on the Aqua SMP server."
+    }
+
+    desc = ""
+    for k, v in command_descs.items():
+        desc += "`{}`: {}\n".format(k, v)
+
+    embed = Embed(
+                title="Commands",
+                color=Color.blue(),
+                description=desc
+    )
+
+    await ctx.send(embed=embed)
+
+
 with open("token.txt", "r") as f:
     lines = f.readlines()
     token = lines[0].strip()
